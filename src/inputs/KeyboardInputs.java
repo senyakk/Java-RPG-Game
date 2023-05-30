72b237071ab9296f13670e75dcb18f2ca59dc856
senyakk <arsenij@arsenij.com>
2023-05-30 12:34:13 +0200

    Controller added

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -1,9 +1,16 @@
 package inputs;
 
+import main.GamePanel;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyboardInputs implements KeyListener {
+
+    private GamePanel gamePanel;
+    public KeyboardInputs(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -17,7 +24,20 @@ public class KeyboardInputs implements KeyListener {
     @Override
     public void keyReleased(KeyEvent e) {
 
-        System.out.println(("A key is pressed"));
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W:
+                gamePanel.changeYDelta(-5);
+                break;
+            case KeyEvent.VK_A:
+                gamePanel.changeXDelta(-5);
+                break;
+            case KeyEvent.VK_S:
+                gamePanel.changeYDelta(+5);
+                break;
+            case KeyEvent.VK_D:
+                gamePanel.changeXDelta(+5);
+                break;
+        }
 
     }
 }
